eeea16bbd3b836bb79b7c379856d03edad4c1681
Will Peters <68400703+wrpeters@users.noreply.github.com>
2022-12-15 18:22:34 -0500

    Pushing small changes to get values from form inputs

---
+++ script.js
@@ -0,0 +1,19 @@
+// Onclick handler functions for encrypt and decrypt
+document.getElementById('submitEncrypt').addEventListener("click", encryptText);
+document.getElementById('submitDecrypt').addEventListener("click", decryptText);
+
+// Encrypts text given user's key
+function encryptText(){
+    console.log('In Encrypt');
+    var keyText = document.getElementById('keyText');
+    console.log('Key Text: ' + keyText.value);
+
+}
+
+// Decrypts text given a user's key
+function decryptText(){
+    console.log('In Decrypt');
+    var keyText = document.getElementById('keyText');
+    console.log('Key Text: ' + keyText.value);
+}
+
