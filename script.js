fcd1535bbc9e5f0175357f273faaa187e1903cbc
Will Peters <68400703+wrpeters@users.noreply.github.com>
2022-12-15 19:13:55 -0500

    changes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-import EncryptRsa from 'encrypt-rsa';
+// const EncryptRsa = require('./encrypt-rsa/build/index.js')
+// // import EncryptRsa from './encrypt-rsa/index';
 
-// create rsa instance
-const encryptRsa = new EncryptRsa();
+// // Create rsa instance
+// const encryptRsa = new EncryptRsa();
+
+// // Create Private and Public keys
+// const { privateKey, publicKey } = nodeRSA.createPrivateAndPublicKeys();
 
 // Onclick handler functions for encrypt and decrypt
 document.getElementById('submitEncrypt').addEventListener("click", encryptText);
@@ -12,6 +16,9 @@ function encryptText(){
     console.log('In Encrypt');
     var keyText = document.getElementById('keyText');
     console.log('Key Text: ' + keyText.value);
+
+    console.log('whats up!')
+    console.log('encrypted', CryptoJS.AES.encrypt('themessage', 'thekey'))
 
 }
 
